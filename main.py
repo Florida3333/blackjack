2a5780af0a586256cb40eb61301a4d105a5bcb7e
Danil <ded4ida@gmail.com>
2024-10-22 20:15:55 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from blackjack import blackjack
+
+# # If the program is run (instead of imported), run the game:
+# if __name__ == '__main__':
+#     blackjack.runGame()
+print(blackjack.getDeck())
